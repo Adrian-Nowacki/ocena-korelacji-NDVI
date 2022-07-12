9a893322b34f5bddc2ec3ca6d3e16de248c15b14
anowacki <adriannowacki007@gmail.com>
2022-07-12 15:42:34 +0200

    dodano kolejny wykres

--- wykresy.R
+++ wykresy.R
@@ -13,11 +13,9 @@ ndvi_WPN <- raster("dane_gotowe/ndvi_WPN.tif")
 temp_WPN <- raster("dane_gotowe/temp_WPN.tif")
 ndvi_WPN <- raster("ndvi_probne.tif")
 temp_WPN <- raster("temp_probne.tif")
-grupa <- c(ndvi_WPN, temp_WPN)
 
 # podglad warstwy
 tm_shape(ndvi_miedzyzdroje) + tm_raster()
-tm_shape(cor) + tm_raster()
 
 # zapisanie rastra do ramki danych
 ndvi_miedzyzdroje_dt <- values(ndvi_miedzyzdroje)
@@ -32,38 +30,60 @@ ndvi_WPN_dt <- as.data.frame(ndvi_WPN_dt) %>% na.omit()
 temp_WPN_dt <- values(temp_WPN)
 temp_WPN_dt <- as.data.frame(temp_WPN_dt) %>% na.omit()
 
-# stworzenie histogramu
-p = ggplot(as.data.frame(ndvi_miedzyzdroje_dt), aes(x = ndvi_miedzyzdroje_dt)) +
-  geom_histogram(fill = hcl.colors(40, palette = "RdYlGn"), bins = 40) +
-  labs(title = "piwo", x = "Wskaznik NDVI", y = "Liczba") +
-  theme(plot.title = element_text(hjust = 0.5, face = "bold")) + 
-  theme(
-    panel.grid.major.x = element_blank(),
-    plot.background = element_rect(fill = "#222222"),
-    panel.background = element_rect(fill = "#222222"), 
-    axis.title = element_text(size = 15,
-                              color = "#dddddd"), 
-    plot.title = element_text(size = 18,
-                              color = "#dddddd",
-                              vjust = 2,
-                              hjust = 0.5), 
-    legend.background = element_rect(color = "#222222", 
-                                     fill = "#777777"),  
-    legend.title = element_text(size = 13),
-    legend.text = element_text(size = 12),
-    axis.text = element_text(size = 12, 
-                             color = "#dddddd"),
-    axis.text.x = element_text(angle = 60, vjust = 0.95, hjust=1))
-ggplotly(p)
-#Sys.setenv("plotly_username" = "adryanqe")
-#Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
-#api_create(p, "NDVI Miedzyzdroje")
 
+# styl wykresow
+style <- theme(
+  panel.grid.major.x = element_blank(),
+  plot.background = element_rect(fill = "#222222"),
+  panel.background = element_rect(fill = "#222222"), 
+  axis.title = element_text(size = 15,
+                            color = "#dddddd"), 
+  plot.title = element_text(size = 18,
+                            color = "#dddddd",
+                            vjust = 2,
+                            hjust = 0.5), 
+  legend.background = element_rect(color = "#222222", 
+                                   fill = "#777777"),  
+  legend.title = element_text(size = 13),
+  legend.text = element_text(size = 12),
+  axis.text = element_text(size = 12, 
+                           color = "#dddddd"),
+  axis.text.x = element_text(angle = 60, vjust = 0.95, hjust=1))
 
+# wyodrebnienie polowy danych ze wzgledu na zbyt duzy rozmiar pliku do eksportu
 
-# korelacje
-cor <- layerStats(temp_WPN, "pearson", ndvi_WPN, na.rm = T)
+ndvi_WPN_dt <- ndvi_WPN_dt[seq(1, nrow(ndvi_WPN_dt), 10), ]
+ndvi_WPN_dt <- as.data.frame(ndvi_WPN_dt)
+
+# stworzenie histogramu NDVI dla Miedzyzdrojow
+  histogram_miedzyzdroje = ggplot(as.data.frame(ndvi_miedzyzdroje_dt), aes(x = ndvi_miedzyzdroje_dt)) +
+    geom_histogram(fill = hcl.colors(40, palette = "RdYlGn"), bins = 40) +
+    labs(title = "piwo", x = "Wskaznik NDVI", y = "Liczba") +
+    theme(plot.title = element_text(hjust = 0.5, face = "bold")) + style
+  ggplotly(histogram_miedzyzdroje)
+  
+  #Sys.setenv("plotly_username" = "adryanqe")
+  #Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
+  #api_create(p, "NDVI Miedzyzdroje")
+
+  
+  # stworzenie histogramu NDVI dla WPN
+  histogram_WPN = ggplot(as.data.frame(ndvi_WPN_dt), aes(x = ndvi_WPN_dt)) +
+    geom_histogram(fill = hcl.colors(40, palette = "RdYlGn"), bins = 40) +
+    labs(title = "piwo", x = "Wskaznik NDVI", y = "Liczba") +
+    theme(plot.title = element_text(hjust = 0.5, face = "bold")) + style
+  ggplotly(histogram_WPN)
+  #Sys.setenv("plotly_username" = "adryanqe")
+  #Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
+  #api_create(p, "NDVI Miedzyzdroje")
+  
+  
+
+######## KORELACJE NDVI Z TEMPERATURA GRUNTU
+# korelacja dla obszaru WPN
 cor(temp_WPN_dt, ndvi_WPN_dt, method = "pearson")
+
+# korelacja dla obszaru Miedzyzdrojow
 cor(temp_miedzyzdroje_dt, ndvi_miedzyzdroje_dt, method = "pearson")
 dane <- temp_WPN_dt
 dane$ndvi <- ndvi_WPN_dt$ndvi_WPN_dt
@@ -71,25 +91,22 @@ dane$ndvi <- ndvi_WPN_dt$ndvi_WPN_dt
 dane_miasto <- temp_miedzyzdroje_dt
 dane_miasto$ndvi <- ndvi_miedzyzdroje_dt$ndvi_miedzyzdroje_dt
 
-a <- ggplot(dane_miasto, aes(x = temp_miedzyzdroje_dt, y = ndvi)) + geom_point(aes(fill = "#222222"), colour = "black", shape = 21, size = 2) + xlab("Temperatura") + 
+# wyodrebnienie polowy danych ze wzgledu na zbyt duzy rozmiar pliku do eksportu
+dane_miasto <- as.data.frame(dane_miasto)
+dane_miasto <- dane_miasto[seq(1, nrow(dane_miasto), 2), ]
+
+korelacja_miedzyzdroje <- ggplot(dane_miasto, aes(x = temp_miedzyzdroje_dt, y = ndvi, color = ndvi)) + 
+  geom_point(shape = 21, size = 2, show.legend = FALSE) + xlab("Temperatura [°C]") + 
   ylab("NDVI") + 
+  scale_x_continuous(breaks = c(20, 25, 30, 35)) +
   geom_smooth(method = lm, color="darkred") + 
-  geom_abline(color = 'grey') +  scale_color_manual(values = c('#999999','#E69F00')) +  
-  theme(
-    panel.grid.major.x = element_blank(),
-    plot.background = element_rect(fill = "#222222"),
-    panel.background = element_rect(fill = "#222222"), 
-    axis.title = element_text(size = 15,
-                              color = "#dddddd"), 
-    plot.title = element_text(size = 18,
-                              color = "#dddddd",
-                              vjust = 2,
-                              hjust = 0.5), 
-    legend.background = element_rect(color = "#222222", 
-                                     fill = "#777777"),  
-    legend.title = element_text(size = 13),
-    legend.text = element_text(size = 12),
-    axis.text = element_text(size = 12, 
-                             color = "#dddddd"),
-    axis.text.x = element_text(angle = 60, vjust = 0.95, hjust=1))
-ggplotly(a)
+  geom_abline(color = 'grey') +  style + scale_color_gradient2(midpoint=mean(dane_miasto$ndvi), low="#e63900", mid = "#ffff66",  high="#00802b")
+ 
+ggplotly(korelacja_miedzyzdroje)
+#Sys.setenv("plotly_username" = "adryanqe")
+#Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
+#api_create(korelacja_miedzyzdroje, "Korelacja Miedzyzdroje")
+
+
+
+
