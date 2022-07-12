75007945a430ef21b10e030724ceaf8dcb375339
anowacki <adriannowacki007@gmail.com>
2022-07-12 17:17:50 +0200

    wykresik temperatury mdz

--- wykresy.R
+++ wykresy.R
@@ -81,6 +81,7 @@ temp_WPN_dt <- as.data.frame(temp_WPN_dt)
 
 
   
+  
 ### Histogramy temperatury
   
 # stworzenie histogramu temp dla WPN
@@ -90,13 +91,14 @@ temp_WPN_dt <- as.data.frame(temp_WPN_dt)
     theme(plot.title = element_text(hjust = 0.5, face = "bold")) + style
   ggplotly(hist_temp_WPN)
   
-# stworzenie histogramu temp dla Miedzyzdojow
-  hist_temp_WPN = ggplot(as.data.frame(temp_WPN_dt), aes(x = temp_WPN_dt)) +
+# stworzenie histogramu temp dla Miedzyzdrojow
+  hist_temp_miedzyzdroje = ggplot(as.data.frame(temp_miedzyzdroje_dt), aes(x = temp_miedzyzdroje_dt)) +
     geom_histogram(fill = hcl.colors(40, palette = "viridis"), bins = 40) +
     labs(x = "Temperatura [°C]", y = "Liczba") +
     theme(plot.title = element_text(hjust = 0.5, face = "bold")) + style
-  ggplotly(hist_temp_WPN)
+  ggplotly(hist_temp_miedzyzdroje)
   
+
   
   
   
@@ -140,7 +142,7 @@ ggplotly(korelacja_miedzyzdroje)
 
 #Sys.setenv("plotly_username" = "adryanqe")
 #Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
-#api_create(korelacja_miedzyzdroje, "Korelacja Miedzyzdroje")
+#api_create(hist_temp_miedzyzdroje, "Temperatura Miedzyzdroje")
 
 
 
