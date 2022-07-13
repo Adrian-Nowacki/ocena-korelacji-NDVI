bfe5d1db50b0f5acc085194eda3dae51f6152a8b
anowacki <adriannowacki007@gmail.com>
2022-07-13 20:49:47 +0200

    zmiana własciwosci wykresu

--- wykresy.R
+++ wykresy.R
@@ -20,6 +20,7 @@ tm_shape(ndvi_miedzyzdroje) + tm_raster()
 # zapisanie rastra do ramki danych
 ndvi_miedzyzdroje_dt <- values(ndvi_miedzyzdroje)
 ndvi_miedzyzdroje_dt <- as.data.frame(ndvi_miedzyzdroje_dt) %>% na.omit()
+colnames(ndvi_miedzyzdroje_dt) <- "ndvi"
 
 temp_miedzyzdroje_dt <- values(temp_miedzyzdroje)
 temp_miedzyzdroje_dt <- as.data.frame(temp_miedzyzdroje_dt) %>% na.omit
@@ -33,6 +34,7 @@ temp_WPN_dt <- as.data.frame(temp_WPN_dt) %>% na.omit()
 
 # styl wykresow
 style <- theme(
+  text = element_text(family = "Trebuchet MS"),
   panel.grid.major.x = element_blank(),
   plot.background = element_rect(fill = "#222222"),
   panel.background = element_rect(fill = "#222222"), 
@@ -51,8 +53,6 @@ style <- theme(
   axis.text.x = element_text(angle = 60, vjust = 0.95, hjust=1))
 
 
-
-
 # wyodrebnienie polowy danych ze wzgledu na zbyt duzy rozmiar pliku do eksportu
 
 ndvi_WPN_dt <- ndvi_WPN_dt[seq(1, nrow(ndvi_WPN_dt), 2), ]
@@ -66,12 +66,14 @@ temp_WPN_dt <- as.data.frame(temp_WPN_dt)
 ### Histogramy NDVI
 
 # stworzenie histogramu NDVI dla Miedzyzdrojow
-  histogram_miedzyzdroje = ggplot(as.data.frame(ndvi_miedzyzdroje_dt), aes(x = ndvi_miedzyzdroje_dt)) +
+  histogram_miedzyzdroje = ggplot(ndvi_miedzyzdroje_dt, aes(x = ndvi,
+                                                            text = paste('</br></br><span> Wartosc NDVI: </span>',
+                                                            '<span>', ndvi ,'</span>'))) +
     geom_histogram(fill = hcl.colors(40, palette = "RdYlGn"), bins = 40) +
     labs(title = "piwo", x = "Wskaznik NDVI", y = "Liczba") +
     theme(plot.title = element_text(hjust = 0.5, face = "bold")) + style
-  ggplotly(histogram_miedzyzdroje)
-  
+  ggplotly(histogram_miedzyzdroje, tooltip = paste("text"))
+ 
 # stworzenie histogramu NDVI dla WPN
   hist_ndvi__WPN = ggplot(as.data.frame(ndvi_WPN_dt), aes(x = ndvi_WPN_dt)) +
     geom_histogram(fill = hcl.colors(40, palette = "RdYlGn"), bins = 40) +
@@ -80,7 +82,6 @@ temp_WPN_dt <- as.data.frame(temp_WPN_dt)
   ggplotly(hist_ndvi_WPN)
 
 
-  
   
 ### Histogramy temperatury
   
@@ -117,35 +118,42 @@ cor(temp_miedzyzdroje_dt, ndvi_miedzyzdroje_dt, method = "pearson")
 dane <- temp_WPN_dt
 dane$ndvi <- ndvi_WPN_dt$ndvi_WPN_dt
 
-dane_miasto <- temp_miedzyzdroje_dt
-dane_miasto$ndvi <- ndvi_miedzyzdroje_dt$ndvi_miedzyzdroje_dt
-
+dane_miasto <- round(temp_miedzyzdroje_dt, 1)
+dane_miasto$ndvi <- round(ndvi_miedzyzdroje_dt$ndvi, 2)
+object.size(dane_miasto)
 
 
 # wyodrebnienie polowy danych ze wzgledu na zbyt duzy rozmiar pliku do eksportu
 dane_miasto <- as.data.frame(dane_miasto)
-dane_miasto <- dane_miasto[seq(1, nrow(dane_miasto), 2), ]
+dane_miasto <- dane_miasto[seq(1, nrow(dane_miasto), 3.2), ]
+dane_miasto <- as.data.frame(dane_miasto)
+colnames(dane_miasto) <- c("temp", "ndvi")
 
-korelacja_miedzyzdroje <- ggplot(dane_miasto, aes(x = temp_miedzyzdroje_dt, y = ndvi, color = ndvi)) + 
+korelacja_miedzyzdroje <- ggplot(dane_miasto, aes(x = temp, y = ndvi, color = ndvi)) + 
   geom_point(shape = 21, size = 2, show.legend = FALSE) + xlab("Temperatura [°C]") + 
   ylab("NDVI") + 
   scale_x_continuous(breaks = c(20, 25, 30, 35)) +
   geom_smooth(method = lm, color="darkred") + 
   geom_abline(color = 'grey') +  style + scale_color_gradient2(midpoint=mean(dane_miasto$ndvi), low="#e63900", mid = "#ffff66",  high="#00802b")
- 
+  
+korelacja_miedzyzdroje <- korelacja_miedzyzdroje %>% 
+  style(text = paste('<span style = " font-weight:bold"> Wartość NDVI: </span>',
+                     '<span style = " color:#ffffff">', dane_miasto$ndvi ,'</span>',
+                     '</br></br><span style = "font-weight:bold"> Temperatura [°C]: </span>',
+                     '<span style = " color:#ffffff">', dane_miasto$temp ,'</span>'), traces = 1) %>%
+  style(text = "linia regresji", traces = 2)
+
 ggplotly(korelacja_miedzyzdroje)
 
+  
 
-
+object.size(korelacja_miedzyzdroje)
 
 ### Eksport wykresow do platformy plotly, aby zamiescic w przegladarce
 
-#Sys.setenv("plotly_username" = "adryanqe")
-#Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
-#api_create(hist_temp_miedzyzdroje, "Temperatura Miedzyzdroje")
-
-
-
+Sys.setenv("plotly_username" = "adryanqe")
+Sys.setenv("plotly_api_key" = "BWYeEqc9Tcu65gh28WEw")
+api_create(korelacja_miedzyzdroje, "Korelacja Miedzyzdroje")
 
 
 
@@ -176,3 +184,5 @@ stats(temp_WPN)
 stats(temp_miedzyzdroje)
 
 plot(ndvi_miedzyzdroje)
+
+
